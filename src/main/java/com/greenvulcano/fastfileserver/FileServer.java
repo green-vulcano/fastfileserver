6f60d7012163ea8db5b8fc64def4289d6494c29b
rolag-it <rocco.lagrotteria@gmail.com>
2019-09-30 12:32:14 +0200

    Added delete resource by userid

--- src/main/java/com/greenvulcano/fastfileserver/FileServer.java
+++ src/main/java/com/greenvulcano/fastfileserver/FileServer.java
@@ -18,9 +18,14 @@
  */
 package com.greenvulcano.fastfileserver;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -49,120 +54,156 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class FileServer {
-	
-	 private static String PRIVATE_REPOSITORY = "vault";
-	 private static String PUBLIC_REPOSITORY = "public";
-	 private static String RESOURCE = "media";
-	 private static String resourceBaseDir = ".";
-	 	
-	 public static void main(String[] args) throws Exception {
 
-		    int serverPort = 8080;
-		    resourceBaseDir = ".";
-		 
-		    try {
-		    	if (args!=null && args.length>1) {
-		    		serverPort = Integer.valueOf(args[0]);
-		    		resourceBaseDir = args[1];
-		    	}
-		    } catch (Exception e) {
-				System.err.println("Failed to process arguments");
-				e.printStackTrace();
-			}		    
-		    
-		    System.out.println("Starting Fast File Server for path "+resourceBaseDir+" on port "+serverPort);
-		    
-	        Server server = new Server(serverPort);	        
-	       	       	        
-	        Constraint privateRead = new Constraint();
-	        privateRead.setName("private_read");
-	        privateRead.setAuthenticate(true);
-	        privateRead.setRoles(new String[]{"reader"});
-	        
-	        ConstraintMapping privateReadMapping = new ConstraintMapping();
-	        privateReadMapping.setPathSpec("/" + PRIVATE_REPOSITORY	+ "/*");
-	        privateReadMapping.setConstraint(privateRead);	        
-	        
-	        Constraint edit = new Constraint();
-	        edit.setName("private_edit");
-	        edit.setAuthenticate(true);
-	        edit.setRoles(new String[]{"editor"});
-	        
-	        ConstraintMapping editMapping = new ConstraintMapping();
-	        editMapping.setPathSpec("/"+ RESOURCE);
-	        editMapping.setConstraint(edit);
-	        
-	        List<ConstraintMapping> constraintMappings = new LinkedList<>();	        
-	        constraintMappings.add(privateReadMapping);
-	        constraintMappings.add(editMapping);
-	        
-	        LoginService loginService = new HashLoginService("ffs",
-	                "./ffs.realm.properties");
-	        server.addBean(loginService);
-	        	        
-	        ConstraintSecurityHandler security = new ConstraintSecurityHandler();
-	        security.setConstraintMappings(constraintMappings);
-	        security.setAuthenticator(new BasicAuthenticator());
-	        security.setLoginService(loginService);
-	        
-	        server.setHandler(security);
+    private static String PRIVATE_REPOSITORY = "vault";
+    private static String PUBLIC_REPOSITORY = "public";
+    private static String RESOURCE = "media";
+    private static String resourceBaseDir = ".";
 
-	        ResourceHandler resourceHandler = new ResourceHandler();
-	       
-	        resourceHandler.setDirectoriesListed(true);
-	        resourceHandler.setWelcomeFiles(new String[]{ "index.html" });
-	        resourceHandler.setResourceBase(resourceBaseDir);
+    public static void main(String[] args) throws Exception {
 
-	        ServletHandler servletHandler = new ServletHandler();
-	        servletHandler.addServletWithMapping(FileServlet.class, "/" + RESOURCE);
-	        
-	        HandlerList handlers = new HandlerList();
-	        handlers.setHandlers(new Handler[] { resourceHandler, servletHandler });
-	        security.setHandler(handlers);
+        int serverPort = 8080;
+        resourceBaseDir = ".";
 
-	        server.start();
-	        server.join();
-	    }
-	 
-	 
-	 public static class FileServlet extends HttpServlet {
-		private static final long serialVersionUID = -2448576110248502999L;
+        try {
+            if (args != null && args.length > 1) {
+                serverPort = Integer.valueOf(args[0]);
+                resourceBaseDir = args[1];
+            }
+        } catch (Exception e) {
+            System.err.println("Failed to process arguments");
+            e.printStackTrace();
+        }
 
-		
-		@Override
-		protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-			
-			String contentType = req.getContentType();
-			String userId = req.getParameter("userid");
-			boolean isPublic = Boolean.valueOf(req.getParameter("public")).booleanValue();
-			
-			String mediaName = UUID.randomUUID().toString();
-			
-			if (userId==null) {
-				resp.setStatus(HttpStatus.BAD_REQUEST_400);
-			} else if ("application/json".equalsIgnoreCase(contentType)) {
-				
-				try {
-					String jsondata = req.getReader().lines().collect(Collectors.joining("\n"));				
-					
-					JSONObject media = new JSONObject(jsondata.trim());
-										
-					
-					Path mediaPath = Paths.get(resourceBaseDir, isPublic? PUBLIC_REPOSITORY:PRIVATE_REPOSITORY ,userId, mediaName.concat(".json"));				
-					Files.createDirectories(mediaPath.getParent());
-					
-					Files.write(mediaPath, media.toString().getBytes(), StandardOpenOption.CREATE_NEW);
-					resp.addHeader("Location", Paths.get("/",userId, mediaName.concat(".json")).toString());
-				} catch (JSONException e) {
-					e.printStackTrace();
-					resp.setStatus(HttpStatus.UNPROCESSABLE_ENTITY_422);
-				}
-			} else {
-				resp.setStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE_415);
-			}
-		}		
-		
-		 
-	 }
+        System.out.println("Starting Fast File Server for path " + resourceBaseDir + " on port " + serverPort);
+
+        Server server = new Server(serverPort);
+
+        Constraint privateRead = new Constraint();
+        privateRead.setName("private_read");
+        privateRead.setAuthenticate(true);
+        privateRead.setRoles(new String[] { "reader" });
+
+        ConstraintMapping privateReadMapping = new ConstraintMapping();
+        privateReadMapping.setPathSpec("/" + PRIVATE_REPOSITORY + "/*");
+        privateReadMapping.setConstraint(privateRead);
+
+        Constraint edit = new Constraint();
+        edit.setName("private_edit");
+        edit.setAuthenticate(true);
+        edit.setRoles(new String[] { "editor" });
+
+        ConstraintMapping editMapping = new ConstraintMapping();
+        editMapping.setPathSpec("/" + RESOURCE);
+        editMapping.setConstraint(edit);
+
+        List<ConstraintMapping> constraintMappings = new LinkedList<>();
+        constraintMappings.add(privateReadMapping);
+        constraintMappings.add(editMapping);
+
+        LoginService loginService = new HashLoginService("ffs", "./ffs.realm.properties");
+        server.addBean(loginService);
+
+        ConstraintSecurityHandler security = new ConstraintSecurityHandler();
+        security.setConstraintMappings(constraintMappings);
+        security.setAuthenticator(new BasicAuthenticator());
+        security.setLoginService(loginService);
+
+        server.setHandler(security);
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+
+        resourceHandler.setDirectoriesListed(true);
+        resourceHandler.setWelcomeFiles(new String[] { "index.html" });
+        resourceHandler.setResourceBase(resourceBaseDir);
+
+        ServletHandler servletHandler = new ServletHandler();
+        servletHandler.addServletWithMapping(FileServlet.class, "/" + RESOURCE);
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { resourceHandler, servletHandler });
+        security.setHandler(handlers);
+
+        server.start();
+        server.join();
+    }
+
+    public static class FileServlet extends HttpServlet {
+
+        private static final long serialVersionUID = -2448576110248502999L;
+
+        @Override
+        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+            String contentType = req.getContentType();
+            String userId = req.getParameter("userid");
+            boolean isPublic = Boolean.valueOf(req.getParameter("public")).booleanValue();
+
+            String mediaName = UUID.randomUUID().toString();
+
+            if (userId == null) {
+                resp.setStatus(HttpStatus.BAD_REQUEST_400);
+            } else if ("application/json".equalsIgnoreCase(contentType)) {
+
+                try {
+                    String jsondata = req.getReader().lines().collect(Collectors.joining("\n"));
+
+                    JSONObject media = new JSONObject(jsondata.trim());
+
+                    Path mediaPath = Paths.get(resourceBaseDir, isPublic ? PUBLIC_REPOSITORY : PRIVATE_REPOSITORY, userId, mediaName.concat(".json"));
+                    Files.createDirectories(mediaPath.getParent());
+
+                    Files.write(mediaPath, media.toString().getBytes(), StandardOpenOption.CREATE_NEW);
+                    resp.addHeader("Location", Paths.get("/", userId, mediaName.concat(".json")).toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    resp.setStatus(HttpStatus.UNPROCESSABLE_ENTITY_422);
+                }
+            } else {
+                resp.setStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE_415);
+            }
+        }
+
+        @Override
+        protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+            String userId = req.getParameter("userid");
+
+            if (userId == null) {
+                resp.setStatus(HttpStatus.BAD_REQUEST_400);
+            } else {
+
+                Set<Path> userResources = new HashSet<>();
+                userResources.add(Paths.get(resourceBaseDir, PUBLIC_REPOSITORY, userId));
+                userResources.add(Paths.get(resourceBaseDir, PRIVATE_REPOSITORY, userId));
+
+                resp.setStatus(HttpStatus.NOT_FOUND_404);
+
+                for (Path userRes : userResources) {
+
+                    if (Files.exists(userRes)) {
+                        try {
+                            
+                            Files.walk(userRes, FileVisitOption.FOLLOW_LINKS)
+                                 .sorted(Comparator.reverseOrder())
+                                 .map(java.nio.file.Path::toFile)
+                                 .forEach(File::delete);
+                            
+                        } catch (IOException deleteException) {
+                            deleteException.printStackTrace();
+                            resp.sendError(HttpStatus.SERVICE_UNAVAILABLE_503);
+                            break;
+                        }
+
+                        resp.setStatus(HttpStatus.NO_CONTENT_204);
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
 
 }
